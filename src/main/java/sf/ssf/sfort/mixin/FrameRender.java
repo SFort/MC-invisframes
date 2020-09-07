21faaa5b2f28380d24a99cda4760c7ca4bfa9a75
SFort <github@ssf.tf>
2020-09-07 02:16:07 +0200

    init

---
+++ src/main/java/sf/ssf/sfort/mixin/FrameRender.java
@@ -0,0 +1,15 @@
+package sf.ssf.sfort.mixin;
+
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ItemFrameEntityRenderer.class)
+public abstract class FrameRender {
+	@Redirect(method = "render",at =@At(value = "INVOKE",target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;isInvisible()Z",ordinal = 0))
+	public boolean itemBeInvis(ItemFrameEntity itemFrameEntity) {
+		return itemFrameEntity.isInvisible()||!itemFrameEntity.getHeldItemStack().isEmpty();
+	}
+}
