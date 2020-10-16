526d984c0f95cf9aca52b38540475e80de8c0710
SFort <github@ssf.tf>
2020-10-16 21:48:16 +0200

    Fix #1 & config for ranged visability toggle

--- src/main/java/sf/ssf/sfort/FrameConf.java
+++ src/main/java/sf/ssf/sfort/FrameConf.java
@@ -14,6 +14,7 @@ public class FrameConf implements ModInitializer{
     public static Logger LOGGER = LogManager.getLogger();
 
     public static boolean clientForceInvis = false;
+    public static boolean allowProjectile = false;
 
     @Override
     public void onInitialize() {
@@ -26,7 +27,8 @@ public class FrameConf implements ModInitializer{
             confFile.createNewFile();
             List<String> la = Files.readAllLines(confFile.toPath());
             List<String> defaultDesc = Arrays.asList(
-                    "^-Should client always make frames with items invisible [false] true | false   // You would want to use this if the mod is installed client side only"
+                    "^-Should client always make frames with items invisible [false] true | false   // You would want to use this if the mod is installed client side only",
+                    "^-Allow the use of projectiles for changing visibility [false] true | false //snowballs arrows etc"
             );
             String[] ls = la.toArray(new String[Math.max(la.size(), defaultDesc.size() * 2)|1]);
             for (int i = 0; i<defaultDesc.size();++i)
@@ -34,6 +36,8 @@ public class FrameConf implements ModInitializer{
 
             try{clientForceInvis = ls[0].contains("true");}catch (Exception ignore){}
             ls[0] = String.valueOf(clientForceInvis);
+            try{allowProjectile = ls[2].contains("true");}catch (Exception ignore){}
+            ls[2] = String.valueOf(allowProjectile);
 
             Files.write(confFile.toPath(), Arrays.asList(ls));
             LOGGER.log(Level.INFO,"tf.ssf.sfort.invisframes successfully loaded config file");
--- src/main/java/sf/ssf/sfort/mixin/FrameEntity.java
+++ src/main/java/sf/ssf/sfort/mixin/FrameEntity.java
@@ -1,5 +1,6 @@
 package sf.ssf.sfort.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.decoration.AbstractDecorationEntity;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import sf.ssf.sfort.FrameConf;
 
 @Mixin(ItemFrameEntity.class)
 public abstract class FrameEntity extends AbstractDecorationEntity {
@@ -18,7 +20,8 @@ public abstract class FrameEntity extends AbstractDecorationEntity {
 
 	@Inject(method = "damage",at =@At(value = "INVOKE"),cancellable = true)
 	public void damage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> info) {
-		if (source.getAttacker().isSneaky()) {
+		Entity attacker = source.getAttacker();
+		if (attacker !=null && attacker.isSneaky() && (FrameConf.allowProjectile || source.getName().equals("player"))) {
 			this.setInvisible(!this.isInvisible());
 			info.setReturnValue(true);
 			info.cancel();
